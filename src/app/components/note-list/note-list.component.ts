ab372c7b8aac8b09cdb42ed52e5cf2ff91d81a60
Motjiang <motjiangbrilliant@gmail.com>
2024-12-19 16:25:55 +0200

    Added the method to get all notes back from the array

--- src/app/components/note-list/note-list.component.ts
+++ src/app/components/note-list/note-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Note } from 'src/app/interfaces/note';
+import { NoteService } from 'src/app/services/note.service';
 
 @Component({
   selector: 'app-note-list',
   templateUrl: './note-list.component.html',
   styleUrls: ['./note-list.component.scss']
 })
-export class NoteListComponent {
+export class NoteListComponent  implements  OnInit{
 
+  notes:Note[] = [];
+
+  constructor(private noteService:NoteService) {
+
+  }
+
+  ngOnInit(): void {
+     //Test note list here
+    this.noteService.getNotesObservable().subscribe((notes: Note[]) => {
+      this.notes = notes;
+    });
+  }
 }
